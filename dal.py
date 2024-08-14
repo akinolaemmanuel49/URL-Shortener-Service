f9940c6bcfda88f789cf3a48549c96c434969323
Akinola Abiodun Emmanuel <akinolaemmanuel49@gmail.com>
2024-08-14 14:08:00 +0100

    Refactor and add functions for metrics

--- dal.py
+++ dal.py
@@ -18,10 +18,14 @@ async def fetch_key(original_url: HttpUrl, owner_id: str) -> Record:
     Returns:
         Record: The database record containing the key.
     """
-    query = """SELECT key FROM urls WHERE original_url = :original_url AND owner_id = :owner_id"""
-    values = {"original_url": str(original_url), "owner_id": owner_id}
-    result = await db.fetch_one(query=query, values=values)
-    return result
+    _query = """SELECT key FROM urls WHERE original_url = :original_url AND owner_id = :owner_id"""
+    _values = {"original_url": str(original_url), "owner_id": owner_id}
+
+    try:
+        result = await db.fetch_one(query=_query, values=_values)
+        return result
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 async def fetch_original_url(key: str) -> Optional[HttpUrl]:
@@ -34,12 +38,17 @@ async def fetch_original_url(key: str) -> Optional[HttpUrl]:
     Returns:
         Optional[HttpUrl]: The original URL if found, None otherwise.
     """
-    query = """SELECT original_url FROM urls WHERE key = :key"""
-    result = await db.fetch_one(query=query, values={"key": key})
-    if result:
-        return HttpUrl(result["original_url"])
-    else:
-        return None
+    _query = """SELECT original_url FROM urls WHERE key = :key"""
+    _values = {"key": key}
+
+    try:
+        result = await db.fetch_one(query=_query, values=_values)
+        if result:
+            return HttpUrl(result["original_url"])
+        else:
+            return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 async def fetch_multiple_urls(owner_id: str, limit: int = 10, offset: int = 0):
@@ -56,13 +65,13 @@ async def fetch_multiple_urls(owner_id: str, limit: int = 10, offset: int = 0):
                                            and the second element is a list of APIReadResponse objects containing
                                            shortened and original URLs.
     """
-    count_query = """
+    _count_query = """
         SELECT COUNT(*) AS total_count 
         FROM urls 
         WHERE owner_id = :owner_id
     """
 
-    records_query = """
+    _records_query = """
         SELECT key, original_url 
         FROM urls 
         WHERE owner_id = :owner_id 
@@ -70,22 +79,26 @@ async def fetch_multiple_urls(owner_id: str, limit: int = 10, offset: int = 0):
         LIMIT :limit OFFSET :offset 
     """
 
-    count_values = {"owner_id": owner_id}
-    record_values = {"owner_id": owner_id, "limit": limit, "offset": offset}
-    count_result = await db.fetch_one(query=count_query, values=count_values)
-    total_count: int = count_result["total_count"]
+    _count_values = {"owner_id": owner_id}
+    _record_values = {"owner_id": owner_id, "limit": limit, "offset": offset}
 
-    records = await db.fetch_all(query=records_query, values=record_values)
+    try:
+        count_result = await db.fetch_one(query=_count_query, values=_count_values)
+        total_count: int = count_result["total_count"]
 
-    result = [
-        APIReadResponse(
-            shortened_url=f"{settings.SHORTENED_URL_BASE}{record['key']}",
-            original_url=record["original_url"],
-        )
-        for record in records
-    ]
+        records = await db.fetch_all(query=_records_query, values=_record_values)
 
-    return total_count, result
+        result = [
+            APIReadResponse(
+                shortened_url=f"{settings.SHORTENED_URL_BASE}{record['key']}",
+                original_url=record["original_url"],
+            )
+            for record in records
+        ]
+
+        return total_count, result
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 async def create_record(original_url: HttpUrl, owner_id: str, unique_key: str):
@@ -101,23 +114,32 @@ async def create_record(original_url: HttpUrl, owner_id: str, unique_key: str):
         Tuple[str, bool]: The unique key and a boolean indicating if a new record was created.
     """
     # Check if the URL already exists for the owner
-    query_select = """SELECT key FROM urls WHERE original_url = :original_url AND owner_id = :owner_id"""
-    values_select = {"original_url": str(original_url), "owner_id": owner_id}
-    existing_url = await db.fetch_one(query=query_select, values=values_select)
+    _query_select = """SELECT key FROM urls WHERE original_url = :original_url AND owner_id = :owner_id"""
+    _values_select = {"original_url": str(original_url), "owner_id": owner_id}
 
-    if existing_url:
-        # If the URL exists, return the existing shortened key
-        return (str(existing_url["key"]), False)
+    try:
+        existing_url = await db.fetch_one(query=_query_select, values=_values_select)
 
-    # Otherwise, store the key and URL value in the database
-    query_insert = """INSERT INTO urls (key, original_url, owner_id) VALUES (:key, :original_url, :owner_id)"""
-    values_insert = {
-        "key": unique_key,
-        "original_url": str(original_url),
-        "owner_id": owner_id,
-    }
-    await db.execute(query=query_insert, values=values_insert)
-    return (unique_key, True)
+        if existing_url:
+            # If the URL exists, return the existing shortened key
+            return (str(existing_url["key"]), False)
+
+        # Otherwise, store the key and URL value in the database
+        _query_insert = """INSERT INTO urls (key, original_url, owner_id) VALUES (:key, :original_url, :owner_id)"""
+        _values_insert = {
+            "key": unique_key,
+            "original_url": str(original_url),
+            "owner_id": owner_id,
+        }
+
+        try:
+            await db.execute(query=_query_insert, values=_values_insert)
+            return (unique_key, True)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 async def remove_record(key: str, owner_id: str) -> bool:
@@ -132,15 +154,81 @@ async def remove_record(key: str, owner_id: str) -> bool:
         bool: True if the record was deleted, False otherwise.
     """
     # Check if the URL exists in the database
-    query = """SELECT key, original_url FROM urls WHERE key = :key AND owner_id = :owner_id"""
-    values = {"key": key, "owner_id": owner_id}
-    existing_url = await db.fetch_one(query=query, values=values)
+    _query = """SELECT key, original_url FROM urls WHERE key = :key AND owner_id = :owner_id"""
+    _values = {"key": key, "owner_id": owner_id}
 
-    if existing_url:
-        # If the URL exists, delete it
-        query = """DELETE FROM urls WHERE key = :key"""
-        values = {"key": key}
-        await db.execute(query=query, values=values)
-        return True
+    try:
+        existing_url = await db.fetch_one(query=_query, values=_values)
 
-    return False
+        if existing_url:
+            # If the URL exists, delete it
+            _query = """DELETE FROM urls WHERE key = :key"""
+            _values = {"key": key}
+
+            try:
+                await db.execute(query=_query, values=_values)
+                return True
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+        return False
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+async def set_metrics(key: str, **kwargs):
+    _query_select = """SELECT owner_id FROM urls WHERE key = :key;"""
+    _values_select = {"key": key}
+
+    try:
+        result = await db.fetch_one(query=_query_select, values=_values_select)
+
+        if result:
+            owner_id = str(result["owner_id"])
+
+            _query_insert = """INSERT INTO metrics (key, owner_id, client_ip, response_time) VALUES (:key, :owner_id, :client_ip, :response_time);"""
+            _values_insert = {
+                "key": key,
+                "owner_id": owner_id,
+                "client_ip": kwargs["client_ip"],
+                "response_time": kwargs["response_time"],
+            }
+
+            try:
+                await db.execute(query=_query_insert, values=_values_insert)
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+async def count_hits(key: str):
+    """
+    Counts the number of times a shortened URL was resolved
+
+    Args:
+        key (str): The shortened URL key.
+    """
+    _query = """SELECT COUNT(*) AS total_number_of_hits FROM urls WHERE key = :key;"""
+    _values = {"key": key}
+
+    try:
+        count_result = await db.execute(query=_query, values=_values)
+        total_number_of_hits: int = count_result["total_number_of_hits"]
+        return total_number_of_hits
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+async def count_unique_ips(key: str):
+    pass
+
+
+async def evaluate_performance(key: str):
+    pass
+
+
+async def get_metrics(key: str):
+    pass
